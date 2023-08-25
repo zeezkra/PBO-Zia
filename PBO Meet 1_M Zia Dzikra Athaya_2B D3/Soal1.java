b0e3824c2bc46e8b75305269da106d22b958a26a
ZeaZkra <116931402+zeezkra@users.noreply.github.com>
2023-08-25 20:54:07 +0700

    Add files via upload

---
+++ PBO Meet 1_M Zia Dzikra Athaya_2B D3/Soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal1;
+
+/**
+ *
+ * @author IMPERIAL
+ */
+public class Soal1 {
+
+    public static void main(String[] args) {
+        byte angka1 = 125; //merupakan variabel bertipe string
+        byte angka2 = 6; //merupakan variabel 2
+        byte hasil = (byte) (angka1 + angka2); //proses menghitung
+        
+        System.out.println("Hasil 1"+hasil);//output
+    }
+}
---
+++ PBO Meet 1_M Zia Dzikra Athaya_2B D3/Soal2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal2;
+
+/**
+ *
+ * @author IMPERIAL
+ */
+public class Soal2 {
+
+    public static void main(String[] args) {
+        int i = 42;
+            String s = (i<40)?"life":(i>50)?"universe":"everything";//tipe data boolean
+            System.out.println(s);//merupakan output
+    }
+}
